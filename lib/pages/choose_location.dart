c4003371e589e11de324af117057e067ff6bf4f8
Roxanne Reyes <waetechsolutions@gmail.com>
2020-04-30 19:04:39 -0400

    Did some async await data calls

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,17 +6,41 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+    //simulate network request for a username
+  String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Yoshi';
+    });
+
+    //simulate network request for a username
+  String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vegan, musician, egg collector';
+    });
+
+  print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    print('statement');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Text('Choose location screen'),
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Text('hello there'),
     );
   }
 }
+
