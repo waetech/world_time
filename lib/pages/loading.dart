a17a72a33f486e028e174e44e21450ddc57463c2
Roxanne Reyes <waetechsolutions@gmail.com>
2020-05-01 00:55:28 -0400

    Made API request to worldtime.org, converted data date time offset for New York

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,19 +9,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  void getTime() async {
 
- Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
- Map data = jsonDecode(response.body);
- print(data);
- print(data['title']);
+    //Make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/America/New_York');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    print(offset);
+
+    //create a DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
 
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
