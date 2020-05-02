05c41f1c29c71dbf7afd95fac8e086784f647fd0
Roxanne Reyes <waetechsolutions@gmail.com>
2020-05-01 22:20:59 -0400

    Added a cube grid animation for the loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:worldtime/services/world_time.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -33,9 +34,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading...'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitCubeGrid(
+          color: Colors.white,
+          size: 50.0,
+        ),
       ),
     );
   }
